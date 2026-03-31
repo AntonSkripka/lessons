9b6c05f9a531082bd53e9db688012a190575a89e
Antooon32 <antonskripka11@gmail.com>
2026-03-31 18:18:37 +0300

    lesson5-1

--- js/hw-61.js
+++ js/hw-61.js
@@ -101,7 +101,7 @@ console.log(
 //? Отримай масив імен всіх користувачів (поле name).
 const getUserNames = users => {
     //! твій код
-    let userNames = users.map((item) => item.name);
+    const userNames = users.map((item) => item.name);
     return userNames;
 };
 
@@ -118,7 +118,7 @@ console.log(
 //? Отримай масив об'єктів користувачів за кольором очей (поле eyeColor).
 const getUsersWithEyeColor = (users, color) => {
     //! твій код
-    let usersWithEyeColor = users.filter((item) => item.eyeColor === color);
+    const usersWithEyeColor = users.filter((item) => item.eyeColor === color);
     return usersWithEyeColor;
 };
 
@@ -135,7 +135,7 @@ console.log(
 //? Отримати масив імен користувачів за статтю (поле gender).
 const getUsersWithGender = (users, gender) => {
     //! твій код
-    let usersGender = users.filter((item) => item.gender === gender).map((item) => item.name);
+    const usersGender = users.filter((item) => item.gender === gender).map((item) => item.name);
     return usersGender;
 };
 
@@ -152,7 +152,7 @@ console.log(
 //? Отримати масив тільки неактивних користувачів (поле isActive).
 const getInactiveUsers = users => {
     //! твій код
-    let usersActive = users.filter((item) => !item.isActive);
+    const usersActive = users.filter((item) => !item.isActive);
     return usersActive;
 };
 
@@ -169,10 +169,105 @@ console.log(
 //? Отримати користувача (не масив) по email (поле email, він унікальний).
 const getUserWithEmail = (users, email) => {
     //! твій код
-    let userWithEmail = users.find((item) => item.email === email);
+    const userWithEmail = users.find((item) => item.email === email);
     return userWithEmail;
 };
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); //! {об'єкт користувача Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); //! {об'єкт користувача Elma Head}
 console.log("--------------------------------------------------");
+
+
+//todo [Додаткове 14-1]
+console.log(
+    "%c [Додаткове 14-1] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Повернути масив користувачів, вік (збережений у властивості age) яких
+//? потрапляє у заданий діапазон від minAge до maxAge
+const getUsersWithAge = (users, minAge, maxAge) => users.filter(user => user.age >= minAge && user.age <= maxAge)
+//! твій код
+
+console.log(getUsersWithAge(users, 20, 30)); //! [об'єкт Ross Vazquez, об'єкт Elma Head, об'єкт Carey Barr]
+console.log(getUsersWithAge(users, 30, 40)); //! [об'єкт Moore Hensley, об'єкт Sharlene Bush, об'єкт Blackburn Dotson, об'єкт Sheree Anthony]
+console.log("--------------------------------------------------");
+
+//todo [Додаткове 14-2]
+console.log(
+    "%c [Додаткове 14-2] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? З'ясувати, чи всі користувачі зараз активні (властивість isActive)
+//? та повернути true, якщо так або false, якщо ні.
+const isEveryUserActive = users => users.every(user => user.isActive);
+
+console.log("Чи всі користувачі зараз активні? -", isEveryUserActive(users)); //! false
+console.log("--------------------------------------------------");
+
+//todo [Додаткове 14-3]
+console.log(
+    "%c [Додаткове 14-3] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? З'ясувати, чи є хоча б однин активний користувач (властивість isActive)
+//? та повернути true, якщо так або false, якщо ні.
+const isAnyUserActive = users => users.some(user => user.isActive);
+
+console.log("Чи є хоча б однин активний користувач? -", isAnyUserActive(users)); //! true
+
+//todo [Додаткове 14-4]
+console.log(
+    "%c [Додаткове 14-4] ",
+    "color: yellow; background-color: #2274A5",
+);
+//? Є початковий масив об'єктів [arr] 
+//? і масив [arrId] з виборкою інедксів. 
+//? Треба створити вихідній масив [arrByID], 
+//? в якому будуть тільки об'єкти з властивістю id,
+//? яка дорівнює значенням єлементів масива [arrId] 
+const arr = [
+    { id: "id-1" },
+    { id: "id-2" },
+    { id: "id-3" },
+    { id: "id-4" },
+    { id: "id-5" },
+    { id: "id-6" },
+    { id: "id-7" },
+    { id: "id-8" },
+    { id: "id-9" },
+    { id: "id-10" },
+    { id: "id-11" },
+];
+const arrId = ["id-3", "id-5", "id-8", "id-11",];
+//! твій код
+
+const arrFilter = (arr, arrId) => arr.filter((element) => arrId.some(item => item === element.id));
+const arrByID = arrFilter(arr, arrId);
+console.log(arrByID);
+
+const arrByID2 = arrId.flatMap(item => arr.filter(el => el.id === item));
+console.log("arrByID", arrByID2);
+
+let arrByID3 = [];
+for (const arrOb of arr) {
+    for (let i = 0; i < arrId.length; i++) {
+        if (arrOb.id === arrId[i]) {
+            arrByID3.push(arrOb);
+        };
+    };
+};
+console.log(arrByID3);
+
+//? Сформувати початковий масив arrayIn будь якої довжини, наприклад, m=13,
+//? та розділити його на масиви по n=5 елементів,
+//? і скласти з них вихідний масив масивів finalArr
+let arrayIn = [];
+const m = 13;
+// const m = 21;
+const n = 5;
+// const n = 3;
+//! Код виконаного завдання
+//todo-1: Використовуємо готовий масив:
+// arrayIn = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130];
+
+//todo-2: Формуємо масив з m елементів:
